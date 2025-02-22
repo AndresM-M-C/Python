87d2da078df31cecf5552632ebf20af7b339350b
AndresM-M-C <martinezcruzmanuel123@gmail.com>
2025-02-21 19:08:45 -0600

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+x=int(input ("dime que tabla deseas"))
+a=10
+while a >= 1: #ciclo
+    print (f"{x} * {a} = {x*a}")
+    a-=1
+
+    #
+    # hola   
+    '''hola mundo
+    entero de mexico 
+    y sus alrededores'''
