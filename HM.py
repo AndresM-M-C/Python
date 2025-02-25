f2a8192e058d4bed81998b49c51657edb8f048db
AndresM-M-C <martinezcruzmanuel123@gmail.com>
2025-02-24 20:54:16 -0600

    Add files via upload

---
+++ HM.py
@@ -0,0 +1,13 @@
+n1= float(input("ingresa el primer numero ----->"))
+n2= float(input("ingresa el segundo numero ----->"))
+s=n1+n2
+print ("el resultado es ----->", s)
+
+nombre=(input("dime tu nombre completo ----->"))
+print("tu nombre es ----->", nombre)
+#hola
+
+ap=(input("dime tu apellido paterno ----->"))
+ap=(input("dime tu apellido materno ----->"))
+print (nombre + " " + ap + " " + am)
+print("hola mundo")
