d0f8ee6a7809f70b4c5b30d4e96dacf47e85cfd6
AndresM-M-C <martinezcruzmanuel123@gmail.com>
2025-08-19 20:51:12 -0600

    Add files via upload

---
+++ grafico4.py
@@ -0,0 +1,56 @@
+import tkinter as Tk #alias Tk
+
+def suma():
+    num1=int(entry_num1.get()) #entry_num1 es la caja de texto 1
+    num2=int(entry_num2.get()) #entry_num2 es la caja de texto 1
+    resultado=num1+num2
+    label_resultado.config(text="resultado" + str(resultado)) #asigna el
+
+def resta():
+    num1=int(entry_num1.get()) #entry_num1 es la caja de texto 1
+    num2=int(entry_num2.get()) #entry_num2 es la caja de texto 1
+    resultado=num1-num2
+    label_resultado.config(text="resultado" + str(resultado)) #asigna el
+
+def multiplicacion():
+    num1=int(entry_num1.get()) #entry_num1 es la caja de texto 1
+    num2=int(entry_num2.get()) #entry_num2 es la caja de texto 1
+    resultado=num1*num2
+    label_resultado.config(text="resultado" + str(resultado)) #asigna el
+
+def division():
+    num1=int(entry_num1.get()) #entry_num1 es la caja de texto 1
+    num2=int(entry_num2.get()) #entry_num2 es la caja de texto 1
+    resultado=num1/num2
+    label_resultado.config(text="resultado" + str(resultado)) #asigna el
+
+app = Tk.Tk() #ventana o formulario= app el alias Tk
+app.title("TECNOLOGICO DE ESTUDIOS SUPERIORES DE JILOTEPEC")
+
+label_num1=Tk.Label(text="primer numero") #label_num1 es de tipo label
+entry_num1=Tk.Entry() # capturar el primer valor
+
+label_num2=Tk.Label(text="segundo numero")#label_num2 es de tipo label
+entry_num2=Tk.Entry() #capturar el segundo valor
+
+label_resultado = Tk.Label(text=" *****") #label resultado es de tipo
+button_suma= Tk.Button(text="sumar", command=suma) #boton_suma es de tipo 
+button_resta= Tk.Button(text="restar", command=resta) #boton_suma es de tipo 
+button_multiplicacion= Tk.Button(text="multiplicar", command=multiplicacion) #boton_suma es de tipo 
+button_division= Tk.Button(text="dividir", command=division) #boton_suma es de tipo 
+
+
+label_num1.pack() #empaquetar y mostrar label_num1 en pantalla
+entry_num1.pack() #empaquetar y mostrar la caja de texto entry_num1 en pantalla
+ 
+label_num2.pack()
+entry_num2.pack()
+
+label_resultado.pack()
+button_suma.pack()
+button_resta.pack()
+button_multiplicacion.pack()
+button_division.pack()
+
+app.geometry("500x500")
+app.mainloop()
